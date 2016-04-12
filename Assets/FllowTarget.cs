6c0cf1e47db1fc731be2e27e4ce46f10128a3581
Michael <zhdelong@foxmail.com>
2016-04-12 11:20:08 +0800

    PlayerMovementOk

---
+++ Assets/FllowTarget.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FllowTarget : MonoBehaviour {
+
+    public float smoothing = 3;
+    private Transform player;
+	// Use this for initialization
+	void Start () {
+        player = GameObject.FindGameObjectWithTag(Tags.player).transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 targetPos = player.position + new Vector3(0, 6, -8);
+        transform.position = Vector3.Lerp(transform.position, targetPos, smoothing * Time.deltaTime);
+	}
+}
