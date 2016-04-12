4d55cd46d0bcb210eb33587351e1e0e35208bc21
Michael <zhdelong@foxmail.com>
2016-04-12 15:30:27 +0800

    LeadByMouseOK

--- Assets/PlayerMove.cs
+++ Assets/PlayerMove.cs
@@ -7,25 +7,41 @@ public class PlayerMove : MonoBehaviour {
 
     private Rigidbody _rigidbody;
     private Animator anim;
+    private int groundLayerIndex = -1;
 	// Use this for initialization
 	void Start () {
         _rigidbody = GetComponent<Rigidbody>();
         anim = GetComponent<Animator>();
+
+        groundLayerIndex = LayerMask.GetMask("Ground");
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
+
+        //控制移动
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
 
         //transform.Translate(new Vector3(h, 0, v) * speed*Time.deltaTime);
         _rigidbody.MovePosition(transform.position + new Vector3(h, 0, v) * speed * Time.deltaTime);
 
+        //控制转向
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hitInfo;
+        if (Physics.Raycast(ray, out hitInfo, 200, groundLayerIndex)) {
+            Vector3 target = hitInfo.point;
+            target.y = transform.position.y;
+            transform.LookAt(target);
+
+        }
+
+        //控制动画
         if (h != 0 || v != 0) {
             anim.SetBool("Move", true);
         } else {
             anim.SetBool("Move", false);
-
         }
+
 	}
 }
