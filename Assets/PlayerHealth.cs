cf36b48ba6e47ce607a80412c2099ad8388d5509
Michael <zhdelong@foxmail.com>
2016-04-13 10:42:47 +0800

    HeroCanShootWithSoundAndBulltetTrace

--- Assets/PlayerHealth.cs
+++ Assets/PlayerHealth.cs
@@ -4,23 +4,38 @@ using System.Collections;
 public class PlayerHealth : MonoBehaviour {
 
     public float hp = 100;
+    public float smoothing = 5;
     private Animator anim;
     private PlayerMove playMove;
-	// Use this for initialization
-	void Start () {
+    private SkinnedMeshRenderer bodyRender;
+    private PlayerShoot playerShoot;
+
+    void Awake() {
         anim = this.GetComponent<Animator>();
         playMove = this.GetComponent<PlayerMove>();
+        this.bodyRender = GameObject.FindWithTag("SkinnedMeshRendererPlayer").GetComponent<Renderer>() as SkinnedMeshRenderer;
+        playerShoot = this.GetComponentInChildren<PlayerShoot>();
+    }
+	// Use this for initialization
+	void Start () {
+        
 	}
+
+    void FixedUpdate() {
+        if (bodyRender.material.color != Color.white) {
+            bodyRender.material.color = Color.Lerp(bodyRender.material.color,Color.white, smoothing * Time.deltaTime);
+        }
+    }
 
     void Update() {
         if (Input.GetMouseButtonDown(0)) {
-            TakeDamage(30);
+            TakeDamage(20);
         }
     }
     public void TakeDamage(float damage) {
         if (hp <= 0) return;
         this.hp -= damage;
-
+        bodyRender.material.color = Color.red;
         if (hp <= 0) {
             anim.SetBool("Dead", true);
             Dead();
@@ -29,5 +44,6 @@ public class PlayerHealth : MonoBehaviour {
 
     void Dead() {
         this.playMove.enabled = false;
+        this.playerShoot.enabled = false;
     }
 }
---
+++ Assets/PlayerShoot.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerShoot : MonoBehaviour {
+
+    public float shootRate = 2;
+    private float timer = 0;
+    private Light light;
+    private ParticleSystem particleSys;
+    private LineRenderer lineRenderer;
+	// Use this for initialization
+	void Start () {
+        light = GetComponent<Light>();
+        particleSys = this.GetComponentInChildren<ParticleSystem>();
+        lineRenderer = this.GetComponent<Renderer>() as LineRenderer;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    timer += Time.deltaTime;
+        if (timer > 1 / shootRate) {
+            timer -= 1/shootRate;
+            Shoot();
+        }
+	}
+
+    void Shoot() {
+        light.enabled = true;
+        particleSys.Play();
+        this.lineRenderer.enabled = true;
+        lineRenderer.SetPosition(0, transform.position);
+        Ray ray = new Ray(transform.position, transform.forward);
+        RaycastHit hitInfo;
+        if (Physics.Raycast(ray, out hitInfo)) {
+            lineRenderer.SetPosition(1, hitInfo.point);
+        } else {
+            lineRenderer.SetPosition(1, transform.position + transform.forward * 100);
+        }
+
+        GetComponent<AudioSource>().Play();
+
+        Invoke("ClearEffect", 0.05f);
+
+    }
+
+    void ClearEffect() {
+        light.enabled = false;
+        lineRenderer.enabled = false;
+    }
+}
