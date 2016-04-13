d6f17363b30527a6e87177ce1bf1985af3615f41
Michael <zhdelong@foxmail.com>
2016-04-13 20:50:41 +0800

    SikiPartCompleted

---
+++ Assets/Spawn.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawn : MonoBehaviour {
+
+    public GameObject enemyPrefab;
+    public float timeSpawnEnemy = 3;
+    public float timer = 0;
+
+
+    void Start() {
+        InvokeRepeating("AccelerateSpawn", 0, 1);
+    }
+    void Update() {
+        timer += Time.deltaTime;
+        if (timer >= timeSpawnEnemy) {
+            timer -= timeSpawnEnemy;
+            SpawnEnemy();
+        }
+    }
+
+    void SpawnEnemy() {
+        GameObject.Instantiate(enemyPrefab, transform.position, transform.rotation);
+    }
+
+    void AccelerateSpawn() {
+        timeSpawnEnemy -= 0.05f;
+    }
+}
